faf939a1b058a2bfaaa836dbd79f5d2075a037b7
Veronica Isabel Herrera <47429735+veroisabelherrera@users.noreply.github.com>
2020-02-20 16:52:23 -0300

    Feat/filter bar - sección titulo, botón nuevo aviso y selector de filtro (#5)
    * añadida dependencias de reactfire
    
    * datos empresa
    
    *  formulario datos basicos
    
    * components forms
    
    * cambiado color de fondo del body en app.css
    
    * añadidos cambios desde el repo de Cynthia
    
    * añadido crear perfil empresa y crear aviso con useReducer, css de crear aviso en curso
    
    * subiendo estilos de modulo derecho de pantalla login (#9)
    
    Aprobado merge, sin conflictos para subir
    
    * primer intento de filter bar
    
    * subiendo barra titulo y selector de filtro
    
    * sección titulo, boton nuevo aviso y selector de filtro
    
    Co-authored-by: Cecilia Geraldo <53872972+ceciliaGeraldo@users.noreply.github.com>
    Co-authored-by: cynthiagomezmontoya <47512559+cynthiagomezmontoya@users.noreply.github.com>
    Co-authored-by: Paloma Zelada <53873028+PalomaZelada@users.noreply.github.com>

--- src/assets/views/Home.jsx
+++ src/assets/views/Home.jsx
@@ -4,7 +4,6 @@ import Footer from '../components/defaultComponents/Footer';
 import Header from '../components/defaultComponents/Header';
 import Content from '../components/defaultComponents/Content';
 
-
 const Home = () => (
   <div className="col-12">
     <Header />
